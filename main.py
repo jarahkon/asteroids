5b7ce73f623e5239590d846272c981e6622b68ad
jarahkon <10908382+jarahkon@users.noreply.github.com>
2025-05-04 13:18:06 +0300

    Refactor main

--- main.py
+++ main.py
@@ -12,14 +12,13 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while (True):
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
-        time_passed = game_clock.tick(60)
-        dt = time_passed / 1000
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
